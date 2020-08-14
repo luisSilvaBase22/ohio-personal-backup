6bc32e894c22c8d8afb5c61a0235a11c7e80214b
Luis Silva <luis.silva@base22.com>
2020-08-14 16:18:19 -0500

    Add anchors to subsections

--- Medicaid/Common Components/Navigation Component Mobile/menu-component-mobile.js
+++ Medicaid/Common Components/Navigation Component Mobile/menu-component-mobile.js
@@ -1,4 +1,5 @@
 (function(){
+	var $secondNavigationMobileContainer = document.querySelector('.b-mega-menu-mobile');
 	var $secondNavigationButton = document.querySelector('.js-btn-display-secondary-pages');
 	var $containerSecondaryPages = document.querySelector('.b-mega-menu-mobile__secondary-pages');
 
@@ -6,16 +7,28 @@
 	$secondNavigationButton.addEventListener('click', function( e ) {
 		var target = e.target;
 
+		var $bottomLine = $secondNavigationMobileContainer.querySelector('.b-mega-menu-mobile__bottom-line');
+
 		var $iconButton = $secondNavigationButton.querySelector('i');
 
 		if ( $containerSecondaryPages.style.display === "none" ) {
 			$containerSecondaryPages.style.display = "block";
+
+			$bottomLine.style.display = "block";
+
 			$iconButton.classList.toggle('fa-minus-circle');
 			$iconButton.classList.toggle('fa-plus-circle');
+
+			$secondNavigationMobileContainer.classList.toggle('js-shadow-box');
 		} else {
 			$containerSecondaryPages.style.display = "none";
+
+			$bottomLine.style.display = "none";
+
 			$iconButton.classList.toggle('fa-plus-circle');
 			$iconButton.classList.toggle('fa-minus-circle');
+
+			$secondNavigationMobileContainer.classList.toggle('js-shadow-box');
 		}
 
 
