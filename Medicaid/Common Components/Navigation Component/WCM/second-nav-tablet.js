e454c9354cf19fbd37c20d4e070ddc137049fb17
Luis Silva <luis.silva@base22.com>
2020-08-21 11:55:26 -0500

    Add scroll to tablet script and make more reuseable code

--- Medicaid/Common Components/Navigation Component/WCM/second-nav-tablet.js
+++ Medicaid/Common Components/Navigation Component/WCM/second-nav-tablet.js
@@ -1,115 +1,122 @@
 document.addEventListener('DOMContentLoaded', function(  ) {
-	var $headerAnchors = document.querySelector('.awesome-navigator.navList1');
 
-	//var $allNavigationAnchors = $headerAnchors.querySelectorAll('li a');
+	var userAgent = navigator.userAgent.toLowerCase();
+	var isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
+	console.log(isTablet);
 
-	var $anchorAboutUs = $('.awesome-navigator.navList1 li:nth-child(2)');
-	var $anchorMedicaid = $('.awesome-navigator.navList1 li:nth-child(3)');
-	var $anchorProviders = $('.awesome-navigator.navList1 li:nth-child(4)');
-	var $anchorIndividuals = $('.awesome-navigator.navList1 li:nth-child(5)');
+	if ( isTablet ) {
+		var $headerAnchors = document.querySelector('.awesome-navigator.navList1');
 
-	var $megaMenuContainer = document.querySelector('.b-mega-menu');
+		//var $allNavigationAnchors = $headerAnchors.querySelectorAll('li a');
 
-	var $imageSectionAboutUs = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Our Structure about us"]');
-	var $imageSectionMedicaid = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Learn About Medicaid"]');
-	var $imageSectionProviders = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Resources for Providers"]');
-	var $imageSectionIndividuals = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Individuals & Families"]');
+		var $anchorAboutUs = $('.awesome-navigator.navList1 li:nth-child(2)');
+		var $anchorMedicaid = $('.awesome-navigator.navList1 li:nth-child(3)');
+		var $anchorProviders = $('.awesome-navigator.navList1 li:nth-child(4)');
+		var $anchorIndividuals = $('.awesome-navigator.navList1 li:nth-child(5)');
 
-	var $containerSectionAboutUs = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Our Structure about us"]');
-	var $containerSectionMedicaid = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Learn About Medicaid"]');
-	var $containerSectionProviders = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Resources for Providers"]');
-	var $containerSectionIndividuals = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Individuals & Families"]');
+		var $megaMenuContainer = document.querySelector('.b-mega-menu');
 
-	var hideMegaMenu = function(){
-		hideAllSections();
-		hideAllImages();
-		$megaMenuContainer.style.display = "none";
-	};
+		var $imageSectionAboutUs = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Our Structure about us"]');
+		var $imageSectionMedicaid = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Learn About Medicaid"]');
+		var $imageSectionProviders = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Resources for Providers"]');
+		var $imageSectionIndividuals = $megaMenuContainer.querySelector('.b-mega-menu-images .b-mega-menu__image-section[data-section="Individuals & Families"]');
 
-	var showMegaMenu = function(){
-		$megaMenuContainer.style.display = "block";
-	};
+		var $containerSectionAboutUs = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Our Structure about us"]');
+		var $containerSectionMedicaid = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Learn About Medicaid"]');
+		var $containerSectionProviders = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Resources for Providers"]');
+		var $containerSectionIndividuals = $megaMenuContainer.querySelector('.b-mega-menu__section .b-section[data-section="Individuals & Families"]');
 
-	var hideAllSections = function(){
-		$containerSectionAboutUs.style.display = "none";
-		$containerSectionMedicaid.style.display = "none";
-		$containerSectionProviders.style.display = "none";
-		$containerSectionIndividuals.style.display = "none";
-	};
+		var hideMegaMenu = function(){
+			hideAllSections();
+			hideAllImages();
+			$megaMenuContainer.style.display = "none";
+		};
 
-	var hideAllImages = function(){
-		$imageSectionAboutUs.style.display = "none";
-		$imageSectionMedicaid.style.display = "none";
-		$imageSectionProviders.style.display = "none";
-		$imageSectionIndividuals.style.display = "none";
-	};
+		var showMegaMenu = function(){
+			$megaMenuContainer.style.display = "block";
+		};
 
-	var lastVisitedSection = "";
+		var hideAllSections = function(){
+			$containerSectionAboutUs.style.display = "none";
+			$containerSectionMedicaid.style.display = "none";
+			$containerSectionProviders.style.display = "none";
+			$containerSectionIndividuals.style.display = "none";
+		};
 
-	var toggleShowHideSections = function( $imageSection, $containerSection ){
+		var hideAllImages = function(){
+			$imageSectionAboutUs.style.display = "none";
+			$imageSectionMedicaid.style.display = "none";
+			$imageSectionProviders.style.display = "none";
+			$imageSectionIndividuals.style.display = "none";
+		};
 
-		hideAllSections();
-		hideAllImages();
-		//hideMegaMenu();
+		var lastVisitedSection = "";
 
-		if ( $megaMenuContainer.style.display === "none" ) {
-			$imageSection.style.display = "block";
-			$containerSection.style.display = "block";
-			lastVisitedSection = $containerSection.dataset.section;
-			showMegaMenu();
-		} else {
-			if ($containerSection.dataset.section !== lastVisitedSection ) {
-				//Switch section in case mega menu is open
+		var toggleShowHideSections = function( $imageSection, $containerSection ){
+
+			hideAllSections();
+			hideAllImages();
+			//hideMegaMenu();
+
+			if ( $megaMenuContainer.style.display === "none" ) {
 				$imageSection.style.display = "block";
 				$containerSection.style.display = "block";
 				lastVisitedSection = $containerSection.dataset.section;
+				showMegaMenu();
 			} else {
-				//Close mega menu
-				hideAllSections();
-				hideAllImages();
-				hideMegaMenu();
-				lastVisitedSection = "";
+				if ($containerSection.dataset.section !== lastVisitedSection ) {
+					//Switch section in case mega menu is open
+					$imageSection.style.display = "block";
+					$containerSection.style.display = "block";
+					lastVisitedSection = $containerSection.dataset.section;
+				} else {
+					//Close mega menu
+					hideAllSections();
+					hideAllImages();
+					hideMegaMenu();
+					lastVisitedSection = "";
+				}
 			}
-		}
-	};
+		};
 
-	$anchorAboutUs.click(function( event ) {
+		$anchorAboutUs.click(function( event ) {
 
-		event.preventDefault();
-		toggleShowHideSections( $imageSectionAboutUs, $containerSectionAboutUs );
-	} );
+			event.preventDefault();
+			toggleShowHideSections( $imageSectionAboutUs, $containerSectionAboutUs );
+		} );
 
-	$anchorMedicaid.click(function( event ) {
+		$anchorMedicaid.click(function( event ) {
 
-		event.preventDefault();
-		toggleShowHideSections( $imageSectionMedicaid, $containerSectionMedicaid );
-	} );
+			event.preventDefault();
+			toggleShowHideSections( $imageSectionMedicaid, $containerSectionMedicaid );
+		} );
 
-	$anchorProviders.click(function( event ) {
+		$anchorProviders.click(function( event ) {
 
-		event.preventDefault();
-		toggleShowHideSections( $imageSectionProviders, $containerSectionProviders );
-	} );
+			event.preventDefault();
+			toggleShowHideSections( $imageSectionProviders, $containerSectionProviders );
+		} );
 
-	$anchorIndividuals.click(function( event ) {
+		$anchorIndividuals.click(function( event ) {
 
-		event.preventDefault();
-		toggleShowHideSections( $imageSectionIndividuals, $containerSectionIndividuals );
-	} );
+			event.preventDefault();
+			toggleShowHideSections( $imageSectionIndividuals, $containerSectionIndividuals );
+		} );
 
-	hideMegaMenu();
+		hideMegaMenu();
 
-	var odxScroll = function() {
+		var odxScroll = function() {
 
-		if( document.documentElement.scrollTop > 30 ) {
-			$megaMenuContainer.style.top = "104px";
-		} else {
-			$megaMenuContainer.style.top = "140px";
-		}
-	};
+			if( document.documentElement.scrollTop > 30 ) {
+				$megaMenuContainer.style.top = "104px";
+			} else {
+				$megaMenuContainer.style.top = "140px";
+			}
+		};
 
-	window.addEventListener("scroll", function() {
-		odxScroll();
-	})
+		window.addEventListener("scroll", function() {
+			odxScroll();
+		})
+	}
 
 });
