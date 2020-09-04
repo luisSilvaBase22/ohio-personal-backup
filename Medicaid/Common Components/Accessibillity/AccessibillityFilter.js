95631d8fcaa212c4cf4bb6316c424a2008e66394
Luis Silva <luis.silva@base22.com>
2020-09-04 15:28:15 -0500

    [FILTER-ACCESSIBILITY] Set in functions and clean code

--- Medicaid/Common Components/Accessibillity/AccessibillityFilter.js
+++ Medicaid/Common Components/Accessibillity/AccessibillityFilter.js
@@ -2,23 +2,18 @@ var AccessibillityScriptsForFilter = {
 	forceFocusArticle: function(){
 		$('.select2-container.select2-container--default').focus();
 	},
-	forceFocusAfterDropdownSelection: function(){
-		var $results = $('.js-b22-results-number');
-		$results.focus();
-	},
+
 	listenerDropdown: function(){
 		var _this = this;
 
 		var $select = $('#js-select-topics');
 		$select.on('select2:select', function( e ) {
 			console.log("SELECTED", e);
-			//_this.forceFocusAfterDropdownSelection();
 			_this.forceFocusArticle();
 			$select.select2('close');
 		});
 		$select.on('change', function( ev ){
 			console.log("DO", ev);
-			//_this.forceFocusAfterDropdownSelection();
 			$select.select2('close');
 		});
 
@@ -45,11 +40,10 @@ var AccessibillityScriptsForFilter = {
 		observer.observe(elemToObserve, {attributes: true});
 	},
 	start: function( articleId ){
-		var $filterWrapper = document.querySelector('article' + articleId );
 
-		//$filterWrapper.setAttribute('aria-label', 'Press space key to display dropdown options');
+		var _this = this;
 
-		$('.select2-container.select2-container--default').attr('aria-label', 'Press space key to display dropdown options');
+		$('.select2-container.select2-container--default').attr('aria-label', 'Press space key to display dropdown options or tab key twice to go to the number of results');
 
 
 		var $inputBox = $('.select2.select2-container');
@@ -68,22 +62,23 @@ var AccessibillityScriptsForFilter = {
 		this.watcherDropdown();
 
 		$inputBox.on('keyup', function( eventInput ){
-			console.log("Pressed");
+			/*console.log("Pressed");
 			if ( eventInput.which === 32 ) {
 				console.log("SPACE");
 			}
+			 */
 
 			//Move input out of the list in order to be found by the Screen Reader
-			var $input = '<input class="select2-search__field" type="search" tabindex="0" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" role="textbox" aria-autocomplete="list" style="width: 0.75em;">';
+			//var $input = '<input class="select2-search__field" type="search" tabindex="0" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" role="textbox" aria-autocomplete="list" style="width: 0.75em;">';
 
-			var $span = $('.select2-search');
+			//var $span = $('.select2-search');
 
-			if ( $span.length < 1 ) {
-				$('.selection .select2-selection').append( '<span class="select2-search select2-search--inline">' + $input + '</span>' );
-			}
+			//if ( $span.length < 1 ) {
+				//$('.selection .select2-selection').append( '<span class="select2-search select2-search--inline">' + $input + '</span>' );
+			//}
 
-			var $listItemAndInput = $('li.select2-search.select2-search--inline');
-			$listItemAndInput.remove();
+			//var $listItemAndInput = $('li.select2-search.select2-search--inline');
+			//$listItemAndInput.remove();
 
 
 		});
