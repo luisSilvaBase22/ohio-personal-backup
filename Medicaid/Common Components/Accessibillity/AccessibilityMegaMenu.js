b3aa3cc97fdc1b2c7a62fa184366f4023f817b18
Luis Silva <luis.silva@base22.com>
2020-09-07 17:34:56 -0500

    [MEGA MENU] Accessibility Add function to focus on next section after being in last subsection link

--- Medicaid/Common Components/Accessibillity/AccessibilityMegaMenu.js
+++ Medicaid/Common Components/Accessibillity/AccessibilityMegaMenu.js
@@ -8,67 +8,66 @@ var AcccessibilityMegaMenu = {
 	},
 	listenerLastItemFromSubsection: function( selectorSublinks, $currentSection, $nextSection ){
 		var _this = this;
-		//var $megaMenu = document.querySelector('.b-mega-menu');
 
 		var $subsectionWrapper = this.$megaMenu.querySelector( selectorSublinks );
 		var $subsectionLinks = $subsectionWrapper.querySelectorAll( '.b-section__secondary-pages-list a' );
-		var $lastLinkFromCurrentSubSection = $subsectionLinks[ $subsectionLinks.length -1 ];
 
-		$lastLinkFromCurrentSubSection.addEventListener('focusout', function(){
+		var templateSpanClosure = '<span tabindex="0" class="js-close-mg" style="width: 0"></span>';
+
+		var $lastLinkFromCurrentSubSection = $subsectionLinks[ $subsectionLinks.length -1 ];
+		$lastLinkFromCurrentSubSection.insertAdjacentHTML('afterbegin', templateSpanClosure);
+
+		var $spanClosure = $lastLinkFromCurrentSubSection.querySelector('span.js-close-mg');
+
+		$spanClosure.addEventListener('focus', function(){
 			if ( $nextSection ) {
+				console.log("Force focus");
 				$nextSection.focus();
-				_this.focusMainNavigation();
 				hideAllSectionsAndMegaMenuContainer();
 				$currentSection.setAttribute('aria-expanded', 'false');
+				$nextSection.focus();
 			} else {
 				_this.focusHelp();
 				hideAllSectionsAndMegaMenuContainer();
 				$currentSection.setAttribute('aria-expanded', 'false');
 			}
+
 		});
 	},
-	focusSubsection: function( selectorSection ){
+	focusSubsection: function( selectorSection, $currentSection, $nextSection ){
 		//var $megaMenu = document.querySelector('.b-mega-menu');
 
 		var $subsectionWrapper = this.$megaMenu.querySelector( selectorSection );
 		var $firstLink = $subsectionWrapper.querySelector( '.b-section__title a' );
 		$firstLink.focus();
-	},
-	focusMainNavigation: function(){
-		var $headerAnchors = document.querySelector('.awesome-navigator.navList1');
-		var $aboutUs = $headerAnchors.querySelector('li:nth-child(2) a');
-		var $medicaid = $headerAnchors.querySelector('li:nth-child(3) a');
-		var $providers = $headerAnchors.querySelector('li:nth-child(4) a');
-		var $individuals = $headerAnchors.querySelector('li:nth-child(5) a');
 
-		$medicaid.focus();
 	},
 	addAriasToMegaMenu: function( $aboutUs, $medicaid, $providers, $individuals ){
 		//var $megaMenu = document.querySelector('.b-mega-menu');
 
 		this.$megaMenu.setAttribute('aria-expanded', 'false');
-		this.$megaMenu.setAttribute('aria-hidden', 'false');
+		//this.$megaMenu.setAttribute('aria-hidden', 'false');
 		this.$megaMenu.setAttribute('role', 'region');
 
-		$aboutUs.setAttribute('aria-hidden', 'false');
+		//$aboutUs.setAttribute('aria-hidden', 'false');
 		$aboutUs.setAttribute('aria-label', 'Our Structure about us');
 		$aboutUs.setAttribute('role', 'button');
 		$aboutUs.setAttribute('aria-expanded', 'false');
 		$aboutUs.setAttribute('tabindex', '0' );
 
-		$medicaid.setAttribute('aria-hidden', 'false');
+		//$medicaid.setAttribute('aria-hidden', 'false');
 		$medicaid.setAttribute('aria-label', 'Learn About Medicaid');
 		$medicaid.setAttribute('role', 'button');
 		$medicaid.setAttribute('aria-expanded', 'false');
 		$medicaid.setAttribute('tabindex', '0' );
 
-		$providers.setAttribute('aria-hidden', 'false');
+		//$providers.setAttribute('aria-hidden', 'false');
 		$providers.setAttribute('aria-label', 'Resources for Providers');
 		$providers.setAttribute('role', 'button');
 		$providers.setAttribute('aria-expanded', 'false');
 		$providers.setAttribute('tabindex', '0' );
 
-		$individuals.setAttribute('aria-hidden', 'false');
+		//$individuals.setAttribute('aria-hidden', 'false');
 		$individuals.setAttribute('aria-label', 'Families and Individuals');
 		$individuals.setAttribute('role', 'button');
 		$individuals.setAttribute('aria-expanded', 'false');
@@ -102,12 +101,10 @@ var AcccessibilityMegaMenu = {
 			event.preventDefault();
 			event.stopPropagation();
 
-			//event.stopImmediatePropagation();
-
 			$aboutUs.setAttribute('aria-expanded', 'true');
 
 			toggleAboutUsSection();
-			_this.focusSubsection( selectorAboutSection );
+			_this.focusSubsection( selectorAboutSection, $aboutUs, $medicaid );
 			_this.listenerLastItemFromSubsection( selectorAboutSection, $aboutUs, $medicaid );
 
 
