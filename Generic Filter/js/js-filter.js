ef40fe63161909672687b815b3a46148e5f8da46
Luis Silva <luis.silva@base22.com>
2020-09-09 14:13:11 -0500

    [FILTER ACCESSIBILITY] Add functions to focus to first card after pagination - Modified global JS of the filter, therefore now this functionality is out of the box. - Remove CACHE from queries. - Remove arrow function

--- Generic Filter/js/js-filter.js
+++ Generic Filter/js/js-filter.js
@@ -67,9 +67,6 @@
 					srv: LocalOptions.AjaxParameters.srv,
 					cmpntid: LocalOptions.AjaxParameters.cmpntid,
 					'WCM_Page.ResetAll': 'TRUE',
-					CACHE: LocalOptions.AjaxParameters.CACHE,
-					CONTENTCACHE: LocalOptions.AjaxParameters.CONTENTCACHE,
-					CONNECTORCACHE: LocalOptions.AjaxParameters.CONNECTORCACHE,
 					location: LocalOptions.AjaxParameters.location ? LocalOptions.AjaxParameters.location : LibrarySettings.wcmLibrary + '/' + LibrarySettings.location,
 				};
 
@@ -84,7 +81,9 @@
 				return 'b' + s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4();
 			},
 			removeDuplicated: function( array ) {
-				return array.filter((a, b) => array.indexOf(a) === b)
+				return array.filter( function( a, b ){
+					return array.indexOf( a ) === b;
+				} );
 			},
 			removeDuplicatedIndex: function( items ){
 				var noRepeatedLetter = '';
@@ -381,7 +380,9 @@
 					}
 				}
 
-				ContentPieces.splice( indexOfItemToRemove, 1 );
+				if ( indexOfItemToRemove ) {
+					ContentPieces.splice( indexOfItemToRemove, 1 );
+				}
 			},
 			getContentPiecesData: function( mappingFunction, sortByDate, AlphabeticalFilter ){
 				var _this = this;
@@ -569,6 +570,7 @@
 				$anchorPagination.attr('href', '#top');
 			},
 			paginateResults: function(page) {
+				var _this = this;
 				var els = this.elements;
 				var cardsContainer = els.$cards.root;
 				var visibleItems = els.visibleItems;
@@ -585,6 +587,32 @@
 				var headerInPage = cardsContainer.find('.js-b22-index-header[data-page="' + page + '"]');
 				headerInPage.show();
 
+				this.removeAriaAttributesFromItemsDisplayed();
+				this.focusFirstItemAfterPagination( itemInPage );
+
+				var $activePage = els.pagination.find('.page-item.active');
+				$activePage.on('click', function() {
+					_this.focusFirstItemAfterPagination( itemInPage );
+				});
+
+			},
+			removeAriaAttributesFromItemsDisplayed: function(){
+				//Accessibility
+				var els = this.elements;
+				var $items = els.$cards.items;
+
+				$items.each( function() {
+					this.removeAttribute('tabindex');
+					this.removeAttribute('aria-hidden');
+				} );
+			},
+			focusFirstItemAfterPagination: function( $items ){
+				//Accessibility
+				if ( $items.length > 0 ) {
+					$items[0].setAttribute('tabindex', '0');
+					$items[0].setAttribute('aria-hidden', 'false');
+					$items[0].focus();
+				}
 			},
 			updateFiltersClicked: function( category, values ){
 				var _this = this;
@@ -1446,6 +1474,7 @@
 					_this.setElements();
 					_this.setListenerCollapseFiltersWhenMobile();
 					_this.setPagination();
+					_this.removeAriaAttributesFromItemsDisplayed();
 					if (deferred)
 						deferred.resolve();
 				} );
